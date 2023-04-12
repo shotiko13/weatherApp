0ef8c10e48d1e332dbc8fa98753ee8d040bd7f96
k3rnel_panic <shotov1303@gmail.com>
2023-04-12 11:49:03 +0400

    added Widget components, visible widget state, remove widget button handler etc.. need to fetch data now.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Widget from "./Widget";
 
 function App() {
   const [location, setLocation] = useState(null);
@@ -12,19 +13,41 @@ function App() {
     };
 
     const handleError = (error) => {
-      console.error('Error obtaining user location:', error);
+      console.error('Error obtaining your location:', error);
     };
 
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(handleSuccess, handleError);
     } else {
-      console.log("Geolocation is not supported by this browser.");
+      console.log("Geoloc not supported, Use another browser.");
     }
   }, []);
 
+  const [visibleCards, SetVisibleCards] = useState([]);
+
+  useEffect(() => {
+    if (location) {
+
+    }
+  }, location);
+
+  const handleRemove = (id) => {
+    SetVisibleCards(visibleCards.filter((card) => card.id != id));
+  };
+
+  const widgets = visibleCards.map((x) => {
+    return (
+      <Widget
+        key={x.id}
+        id={x.id}
+        img={x.img}
+        weather={x.weather}
+        city={x.city} />
+    )
+  })
   return (
     <div className="App">
-      {/*yet empty */}
+      {widgets}
     </div>
   );
 }
--- src/client.js
+++ src/client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 const httpLink = new HttpLink({
-//   uri: 'https://your-graphql-api-endpoint', // empty yet
+//   uri: '', // empty yet
 });
 
 const client = new ApolloClient({
